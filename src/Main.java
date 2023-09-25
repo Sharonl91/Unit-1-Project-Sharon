63b10636303fe15845af4fde7d9d551ae3643475
19176 <l@>
2023-09-25 18:51:22 -0400

    I removed the doubles at the start of the code and decided to plug them in where they're being used, for better viewing. I also removed the Math.round because I found out you could just format the doubles to the hundredth place by changing println to printf and plugging in %.2f.

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        double tipTotal;
-        double total;
-        double tipEach;
-        double totalEach;
 
-        System.out.print("Enter your total bill: ");
+        System.out.print("Enter your total bill: $");
         String bill = s.nextLine();
         double billNew = Double.parseDouble(bill);
-        System.out.print("Enter the tip percentage (as a whole number): ");
+        System.out.print("Enter the tip percentage (as a whole number, e.g., 18 for 18%): ");
         String tipPercent = s.nextLine();
         int tipPercentNew = Integer.parseInt(tipPercent);
         System.out.print("Enter the number of people in your party: ");
         String people = s.nextLine();
-        int peopleNew = Integer.parseInt(people);
+        int numPeople = Integer.parseInt(people);
 
-        tipTotal = (Math.round(billNew * tipPercentNew));
-        tipTotal /= 100;
-        total = (Math.round(tipTotal + billNew));
-        total = (Math.round(tipTotal + billNew));
-        tipEach = (Math.round(tipTotal / peopleNew));
-        tipEach = (Math.round(tipTotal / peopleNew));
-        totalEach = (Math.round(total / peopleNew);
-        totalEach = (Math.round(total / peopleNew);
-        System.out.println("The total tip amount is: " +tipTotal);
-        System.out.println("The total bill including tip is: " +total);
-        System.out.println("The tip per person is: " +tipEach);
-        System.out.println("The total per person is: " +totalEach);
+        double tipTotal = (billNew * tipPercentNew) / 100.0;
+        double total = (tipTotal + billNew);
+        double tipPerPerson = (tipTotal / numPeople);
+        double totalPerPerson = (total / numPeople);
+
+        System.out.printf("The total tip amount is: $%.2f\n",tipTotal);
+        System.out.printf("The total bill including tip is: $%.2f\n", total);
+        System.out.printf("The tip per person is: $%.2f\n", tipPerPerson);
+        System.out.printf("The total per person is: $%.2f\n", totalPerPerson);
     }
 }
